7498ba30a2ea8b302938c01ec066b474b3c7afbf
mdrayhanhowlader <rhowlader5314@gmail.com>
2022-11-08 21:42:38 +0600

    make service api with id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectID } = require('bson');
 require('dotenv').config();
 
 
@@ -31,6 +32,20 @@ async function run (){
             const cursor = serviceCollection.find(query)
             const result = await cursor.limit(3).toArray()
             res.send(result)
+        })
+
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        app.get('/details/:id', async(req, res) => {
+            const id = req.params.id 
+            const query = {_id: ObjectID(id)}
+            const details = await serviceCollection.findOne(query)
+            res.send(details)
         })
 
     }
