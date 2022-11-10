c8ecffe113b87923fc8103cdf5a5af26e6e07b17
mdrayhanhowlader <rhowlader5314@gmail.com>
2022-11-10 18:05:50 +0600

    get data from mongo and updated done

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000
 
 // middleware 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('green focus server is running')
@@ -25,7 +26,7 @@ async function run (){
     try {
         // connect with database and collection 
         const serviceCollection = client.db('greenFocus').collection('services')
-        const reviewsCollection = client.db('greenFocus').collection('reviews')
+        const commentCollection = client.db('greenFocus').collection('reviews')
 
         // get services from database 
         app.get('/home', async(req, res) => {
@@ -42,6 +43,13 @@ async function run (){
             res.send(services)
         })
 
+        app.post('/services', async(req, res) => {
+            const service = req.body
+            const result = await serviceCollection.insertOne(service)
+            res.send(result) 
+        })
+
+
         app.get('/details/:id', async(req, res) => {
             const id = req.params.id 
             const query = {_id: ObjectID(id)}
@@ -50,12 +58,49 @@ async function run (){
         })
 
 
-        // reviews 
-        // app.get('/reviews', async(req, res) => {
-        //     const query = {}
-        //     const cursor = 
-        // })
+        // review insert
+        app.post('/reviews', async(req, res) => {
+            const review = req.body 
+            const reviews = await commentCollection.insertOne(review)
+            res.send(reviews)
+        })
 
+        // review get 
+        app.get('/reviews', async(req, res) => {
+            const query = {}
+            const cursor = commentCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.delete('/reviews/:id', async(req, res) => {
+            const id = req.params.id
+            const query = {_id: ObjectID(id)}
+            const result = await commentCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // edit data 
+        app.get('/edit/:id', async(req, res) => {
+            const id = req.params.id 
+            const query = {_id: ObjectID(id)}
+            const result = await commentCollection.findOne(query)
+            res.send(result)
+        })
+
+        // update data 
+        app.patch('/update/:id', async(req, res) => {
+            const id = req.params.id 
+            const query = {_id: ObjectID(id)}
+            const updateReview = req.body.data 
+            const updateDoc = {
+                $set: {
+                    review: updateReview
+                }
+            }
+            const result = await commentCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
 
     }
     finally {
